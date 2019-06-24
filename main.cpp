889ff0234a02832872e5b444ade1b32cd9348fbb
zefryuuko <titotobing0511@gmail.com>
2019-06-24 22:08:16 +0700

    Added comments

--- main.cpp
+++ main.cpp
@@ -13,18 +13,24 @@
 #include "util/timer.hpp"
 #include "util/mapreader.hpp"
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     if (argc == 1)
     {
         cout << "Usage: maze <file> [algo]" << endl;
+        cout << "Algo: 1. Recursive" << endl;
+        cout << "      2. Depth First Search" << endl;
+        cout << "      3. Breadth First Search" << endl;
         return 1;
     }
     if (argc >= 2)
     {
         MapReader m(argv[1]);
         if (argc >=3)
+            // Read map with user specified algorithm
             m.read(argv[2][0] - 48);
         else
+            // Read map with recursive algorithm if no algorithm parameter is passed.
             m.read(1);
     }
     
